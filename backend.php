f5331ba2ac77230afbe224767fbba50cba8029d6
ZAZW27 <clawsrage27@gmail.com>
2026-02-11 10:30:20 +0800

    new file

---
+++ backend.php
@@ -0,0 +1,6 @@
+<?php 
+
+new mysqli(host, root, password, db); 
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod magnam vero dolores unde pariatur in minus quo maiores ad ut, saepe ab quisquam modi voluptates fugiat. Beatae inventore pariatur sed.
+
+?>
